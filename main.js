eb07ede1c782e6181807b03afbc18adb0de9a9e2
Zoe Ames <zoeames@gmail.com>
2020-02-08 13:36:03 -0600

    wip

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ const testimonials = [
   },
   {
     id: 'testimonial2',
-    imageUrl: 'https://assets.bombas.com/image/fetch/f_auto,q_auto/https://cdn.shopify.com/s/files/1/1119/5850/products/navy-cream-layflat-product_9915ee42-f6ab-4bb5-8315-9f56aae0ebe8_1600x1600.jpg%3Fv%3D1576081488',
+    imageUrl: 'https://whitecompany.scene7.com/is/image/whitecompany/Cashmere-Bed-Socks/CSCBS_79_MAIN_P?$D_PDP_824x1050$',
     name: 'Jane',
     testimonial: 'Sole Mates helped me find the PERFECT pair - even better than the original.'
   },
   {
     id: 'testimonial3',
-    imageUrl: 'https://assets.bombas.com/image/fetch/f_auto,q_auto/https://cdn.shopify.com/s/files/1/1119/5850/products/navy-cream-layflat-product_9915ee42-f6ab-4bb5-8315-9f56aae0ebe8_1600x1600.jpg%3Fv%3D1576081488',
+    imageUrl: 'https://cdn.shopify.com/s/files/1/0104/0030/1120/products/HXW30002I_001_UPC_047852282215_UPC_Black_2000x.png?v=1578521422',
     name: 'Bethanie',
     testimonial: 'Sole Mates helped me find my soul mate!'
   }
@@ -26,8 +26,14 @@ const printToDom = (divId, textToPrint) => {
 
 const buildTestimonials = (arr) => {
   let domString = '';
-  for(let i = 0; i < arr.length; ){
-
+  for(let i = 0; i < arr.length; i++){
+    domString += '<div class="col-3">';
+    domString += `  <img src="${arr[i].imageUrl}" alt=""/>`;
+    domString += '</div>';
+    domString += '<div class="col-9">';
+    domString += `  <h3 class="align-middle">${arr[i].testimonial}</h3>`;
+    domString += `  <h4 class="align-middle">- ${arr[i].name}</h4>`;
+    domString += '</div>';
   }
   printToDom('testimonials', domString);
 };
