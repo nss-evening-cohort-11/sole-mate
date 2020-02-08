8079836c426497e8cd467640a78ed983c92a1904
Mary West <westmarye1219@gmail.com>
2020-02-08 13:40:09 -0600

    fixed everything

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const footerBuilder = () => {
   let domstring = '';
   const date = new Date().getFullYear();
   domstring += `<p class="col-xs-1 text-center">&copy${date}</p>`;
-  console.log(date);
   printToDom('footer-text', domstring)
 }
 
