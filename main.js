4c59eaeba4f3bc4c31e57f815da36b6d31a0a488
Mary West <westmarye1219@gmail.com>
2020-02-08 13:07:09 -0600

    wip

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('my sole mate page');
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+
+
+const footerBuilder = () => {
+  let domstring = '';
+  const date = new Date().getFullYear();
+  domstring += date;
+  printToDom()
+}
+
+
+
